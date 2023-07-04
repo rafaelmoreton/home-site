ffd38fffac0c6764db2da854e8df6f5bbb88b819
Rafael Moreton Baggio <rafaelmoreton@gmail.com>
2023-07-04 18:05:10 -0300

    Show home page on root url
    Create a controller for static pages. It takes the page name as a param
    and uses it to retrieve the corresponding view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get "pages/:page", to: "pages#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#show", page: "home"
 end
